fbf959cb572d1881b0d9a9bd73366df459329604
NikolaevArtem <artem_nikolaev69@mail.ru>
2020-10-04 22:23:12 +0300

    1 and 2 tasks

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,41 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+'use strict'
+
+function getResult(a, b, c) {
+
+    let x;
+    let d = (b ** 2) - (4 * a * c);
+    if (d > 0) {
+        x = [(-b + d) / (2 * a),
+            (-b - d) / (2 * a)
+        ];
+    } else if (d == 0) {
+        x = [-b / (2 * a)];
+    } else if (d < 0) {
+        x = [];
+    }
+    return x
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+
+    let averageMark;
+    let sum = 0;
+    if (marks.length === 0) {
+        averageMark = 0;
+    } else {
+        for (let i = 0; i < marks.length; i++) {
+            if (marks.length > 5) {
+                console.log('Слишком много оценок');
+                marks = marks.slice(0, 5);
+            }
+            sum += marks[i];
+        }
+        averageMark = sum / marks.length
+    }
+    return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){
+function askDrink(name, dateOfBirthday) {
     // код для задачи №3 писать здесь
     // return result;
 }
