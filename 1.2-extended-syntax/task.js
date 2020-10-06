a8d3245e26948a8578fb63a5b0123bac36320de0
NikolaevArtem <artem_nikolaev69@mail.ru>
2020-10-06 16:22:11 +0300

    fixed

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -2,44 +2,39 @@
 
 function getResult(a, b, c) {
 
-    let x;
+    let x = [];
     let d = (b ** 2) - (4 * a * c);
     if (d > 0) {
-        x = [(-b + d) / (2 * a),
-            (-b - d) / (2 * a)
+        x = [(-b + Math.sqrt(d)) / (2 * a),
+            (-b - Math.sqrt(d)) / (2 * a)
         ];
     } else if (d == 0) {
         x = [-b / (2 * a)];
-    } else if (d < 0) {
-        x = [];
     }
     return x
 }
 
+
 function getAverageMark(marks) {
 
-    let averageMark;
+    let averageMark = 0;
     let sum = 0;
-    if (marks.length === 0) {
-        averageMark = 0;
-    } else {
-        for (let i = 0; i < marks.length; i++) {
-            if (marks.length > 5) {
-                console.log('Слишком много оценок');
-                marks = marks.slice(0, 5);
-            }
-            sum += marks[i];
-        }
+    if (marks.length > 5) {
+        console.log('Слишком много оценок');
+        marks = marks.slice(0, 5);
+    }
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
         averageMark = sum / marks.length
     }
     return averageMark;
 }
 
+
 function askDrink(name, dateOfBirthday) {
+
     let result;
-    let date = new Date()
-    let age = date.getFullYear() - dateOfBirthday.getFullYear()
-    if (age < 18) {
+    if ((new Date().getFullYear() - dateOfBirthday.getFullYear()) < 18) {
         result = `Сожалею, ${ name }, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`
     } else {
         result = `Не желаете ли олд-фэшн, ${ name }`
