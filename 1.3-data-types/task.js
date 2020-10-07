cf71842ef0b44387ecfae3b79012e93610b3b710
NikolaevArtem <artem_nikolaev69@mail.ru>
2020-10-07 14:47:42 +0300

    first task

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,7 +1,17 @@
+'use strict'
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let S = amount - contribution; //Взнос
+    let P = percent / 12; // процентная ставка * 100
+    let n = date.getMonth() - new Date().getMonth() + (12 * (date.getFullYear() - new Date().getFullYear())); //кол-во месяцев
+    let payment = S * (P + P / (((1 + P) ** n) - 1)) / 100; //ежемесячный платёж по процентам
+    payment += S / n; //ежемесячный платёж 
+    let totalAmount = contribution + payment * n; //общая стоимость 
+    alert(payment)
+    return totalAmount.toFixed(2);
 }
+
+
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
