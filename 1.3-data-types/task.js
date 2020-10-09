1cccb960c4d94c270ea6877ce192c240ac80ed56
NikolaevArtem <artem_nikolaev69@mail.ru>
2020-10-09 17:24:28 +0300

    tasks completed

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -6,14 +6,30 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     let n = date.getMonth() - new Date().getMonth() + (12 * (date.getFullYear() - new Date().getFullYear())); //кол-во месяцев
     let payment = S * (P + P / (((1 + P) ** n) - 1)) / 100; //ежемесячный платёж по процентам
     payment += S / n; //ежемесячный платёж 
-    let totalAmount = contribution + payment * n; //общая стоимость 
-    alert(payment)
+    let totalAmount = contribution + payment * n; //общая стоимость
+    if (isNaN(percent)) {
+        return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`
+    }
+    if (isNaN(contribution)) {
+        return `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`
+    }
+    if (isNaN(amount)) {
+        return `Параметр "Общая стоимость" содержит неправильное значение ${amount}`
+    }
     return totalAmount.toFixed(2);
+
 }
 
 
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting = ''
+    if (typeof (name) == 'string' && name != '') {
+        console.log(`Привет, мир! Меня зовут ${name}`)
+        greeting = `Привет, мир! Меня зовут ${name}`
+    } else {
+        console.log('Привет, мир! Меня зовут Аноним')
+        greeting = 'Привет, мир! Меня зовут Аноним'
+    }
+    return greeting;
 }
