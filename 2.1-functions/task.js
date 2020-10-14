a7e31333f96155878ce9d3bbaeccfcfd8a38a084
NikolaevArtem <artem_nikolaev69@mail.ru>
2020-10-14 16:08:41 +0300

    comleted

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -42,3 +42,55 @@ showSolutionsMessage(2, 4, 2)
 
 
 //Задание 2
+
+function getAverageScore(...data){
+data = {
+    algebra: [5, 4],
+    geometry: [5, 4, 3, 3],
+    russian: [3, 4, 3, 5],
+    physics: [5, 4, 3, 5],
+    music: [5, 4, 5,],
+    english: [5, 4, 5, 5, 4, 4],
+    poetry: [5, 4, 5],
+    chemistry: [5, 4, 4, 5],
+    french: [5, 4, 3],
+    average: []
+} 
+function getAverageMark(marks) {
+    let i = 0;
+    let allMarsk = 0;
+    let sum = 0;
+for(let key in data) {
+    i++;
+for(let j = 0; j < data[key].length; j++) {
+    sum +=data[key][j];
+}
+allMarsk += data[key].length
+}
+sum = sum/allMarsk
+
+data.average.push(sum)
+}
+getAverageMark()
+console.log(data)
+}
+getAverageScore()
+
+
+//Задание 3
+
+function getPersonData(secretData) {
+    secretData = {
+    aaa: 0,
+    bbb: 1
+    }
+    function getDecodedValue(secret) {
+        secretData.firstName = secretData.aaa === 0 ? 'Родриго' : 'Эмильо';
+        secretData.lastName = secretData.bbb === 0 ? 'Родриго' : 'Эмильо';
+        delete secretData.aaa
+        delete secretData.bbb
+    }
+    getDecodedValue()
+    console.log(secretData)
+}
+getPersonData()
