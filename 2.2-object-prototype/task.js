252418483e013cd937dc6095f50856d76d4c1310
NikolaevArtem <artem_nikolaev69@mail.ru>
2020-10-16 14:32:31 +0300

    completed

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,4 +1,5 @@
 'use strict'
+
 // Задание 1
 String.prototype.isPalindrome = function() {
 let String = this.toLowerCase().match(/[a-zа-я]/g).reverse()
@@ -9,16 +10,29 @@ if(String.join('') === String.reverse().join('')) {
 }
 }
 
-
-
 //Задание 2
-
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let average = 0;
+    let roundedAverage = 0;
+    for(let i = 0; i < marks.length; i++) {
+        average += marks[i]/(marks.length)
+        roundedAverage = Math.round(average)
+    }
+    return roundedAverage
 }
 
+//Задание 3
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = +new Date();
+    birthday = +new Date(birthday);
+    let dif = now - birthday;
+    let age = dif/(1000*60*60*24*365.25)
+    if (age > 18) {
+        return true
+    } else {
+        return false
+    }
 }
+
+
+
