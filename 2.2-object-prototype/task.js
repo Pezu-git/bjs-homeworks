80fb0306b700f892af91b7d9650379b385494c8d
NikolaevArtem <artem_nikolaev69@mail.ru>
2020-10-17 16:06:08 +0300

    fixed

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,38 +1,35 @@
 'use strict'
 
 // Задание 1
-String.prototype.isPalindrome = function() {
-let String = this.toLowerCase().match(/[a-zа-я]/g).reverse()
-if(String.join('') === String.reverse().join('')) { 
-    return true;
-} else {
-    return false
-}
+String.prototype.isPalindrome = function () {
+    let text = this.toLowerCase().match(/[a-zа-я]/g).reverse();
+    if (text.join('') === text.reverse().join('')) {
+        return true;
+    } else {
+        return false
+    }
 }
 
 //Задание 2
 function getAverageMark(marks) {
-    let average = 0;
-    let roundedAverage = 0;
-    for(let i = 0; i < marks.length; i++) {
-        average += marks[i]/(marks.length)
-        roundedAverage = Math.round(average)
+    let average;
+    let roundedAverage;
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
     }
+    average = sum / marks.length
+    roundedAverage = Math.round(average)
+    
     return roundedAverage
 }
 
 //Задание 3
 function checkBirthday(birthday) {
     let now = +new Date();
-    birthday = +new Date(birthday);
-    let dif = now - birthday;
-    let age = dif/(1000*60*60*24*365.25)
-    if (age > 18) {
-        return true
-    } else {
-        return false
-    }
+    const parsedBirthday = new Date(birthday).getTime()
+    let dif = now - parsedBirthday;
+    let age = dif / (1000 * 60 * 60 * 24 * 365.25);
+    
+    return age > 18
 }
-
-
-
