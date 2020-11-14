56d9fd81daeb97aba076ece6d18e6acb68c8b0e6
NikolaevArtem <artem_nikolaev69@mail.ru>
2020-11-14 11:41:44 +0300

    Fixed

--- 3.3-async/task.js
+++ 3.3-async/task.js
@@ -14,8 +14,8 @@ class AlarmClock {
       return item.id === id;
     })
     if (findId === undefined) {
-      this.alarmCollection.push({ id: id, time: time, callback: callback })
-      console.log(this.alarmCollection)
+      this.alarmCollection.push({ id: id, time: time, callback: callback });
+      console.log(this.alarmCollection);
     } else {
       console.error('Будильник уже поставлен');
     }
@@ -23,40 +23,40 @@ class AlarmClock {
 
   //Удаляет определённый звонок
   removeClock(id) {
-    const filterId = this.alarmCollection.find(item => item.id === id)
+    const filterId = this.alarmCollection.find(item => item.id === id);
       if (filterId) {
-        this.alarmCollection.splice(filterId, 1) 
-        console.log(this.alarmCollection)
-        return true
+        this.alarmCollection.splice(this.alarmCollection.indexOf(filterId), 1) 
+        console.log(this.alarmCollection);
+        return true;
       }
-      return false
+    return false;
   } 
   
   //Текущее время string
   getCurrentFormatTime() {
     let dateNow = new Date();
-    let hours = dateNow.getHours()
-    let minutes = dateNow.getMinutes()
+    let hours = dateNow.getHours();
+    let minutes = dateNow.getMinutes();
     if (hours < 10) {
-      hours = '0' + hours
+      hours = '0' + hours;
     }
     if (minutes < 10) {
-      minutes = '0' + minutes
+      minutes = '0' + minutes;
     }
-    return `${hours}:${minutes}`
+    return `${hours}:${minutes}`;
   }
 
   //Запускает все звонки
   start() {
     function checkClock(ring) {
       if (ring.time === alarm.getCurrentFormatTime()) {
-        ring.callback()
+        ring.callback();
       }
     }
     if (this.timerId === undefined) {
       this.timerId = setInterval(() => {
         this.alarmCollection.forEach(item => checkClock(item))
-      }, 5000)
+      }, 5000);
     }
   }
 
@@ -64,19 +64,19 @@ class AlarmClock {
   stop() {
     if (this.timerId !== undefined) {
       clearInterval(this.timerId);
-      this.timerId === undefined
+      this.timerId === undefined;
     }
   }
 
   //Печатает все звонки
   printAlarms() {
-    this.alarmCollection.forEach(item => console.log(`Будильник №${item.id} заведён на ${item.time}`))
+    this.alarmCollection.forEach(item => console.log(`Будильник №${item.id} заведён на ${item.time}`));
   }
 
   //Удаляет все звонки
   clearAlarms() {
     stop();
-    this.alarmCollection.splice(0, this.alarmCollection.length)
+    this.alarmCollection.splice(0, this.alarmCollection.length);
   }
 
   //Тесты
@@ -90,12 +90,14 @@ class AlarmClock {
     // this.addClock('08:05', () => console.log('Get up for work!'));
     this.start();
     this.printAlarms();
+    this.removeClock(4);
+    this.removeClock(1);
+    this.printAlarms();
     // this.stop();
     // this.clearAlarms();
-
   }
 }
 const alarm = new AlarmClock();
-alarm.testCase()
+alarm.testCase();
   
 
